d7990cd3f95a27059c6dd8a36e051ddc14a8940b
sridevigatiganti <sridevigatiganti@gmail.com>
2020-07-26 12:20:24 +0530

    Updated changes

---
+++ cleancode2/src/main/java/cleancode/Main.java
@@ -0,0 +1,15 @@
+package cleancode;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Interest.calculateSimpleInterest(2000,3,10);
+		Interest.calculateCompoundInterest(2000,3,10,2);
+		
+		
+		Construction_cost.cost_calculator(2000, true, "use_standard");
+
+	}
+
+}
